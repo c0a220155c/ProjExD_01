b54e5fca05021f4b51b4b775d884205b83175f41
keigo <c0a220155c@edu.teu.ac.jp>
2023-09-26 15:36:09 +0900

    練習の3-2

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,8 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    
+    
     
 
 
@@ -19,6 +20,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
